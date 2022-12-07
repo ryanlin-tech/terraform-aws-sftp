aed8c2b755d1007a96f2479209b0ec9f728b985a
Ryan <lin827895450@gmail.com>
2022-12-08 01:10:31 +1100

    bugfix: fix tag bug

--- outputs.tf
+++ outputs.tf
@@ -9,8 +9,3 @@ output "transfer_server_endpoint" {
   value       = join("", aws_transfer_server.transfer_server.*.endpoint)
   description = "The endpoint of the Transfer Server (e.g. s-12345678.server.transfer.REGION.amazonaws.com)."
 }
-
-output "tags" {
-  value       = module.labels.tags
-  description = "A mapping of tags to assign to the resource."
-}
