2e636414c01ae792336de43e2f918c5302747dc1
Ryan <lin827895450@gmail.com>
2022-12-08 01:40:21 +1100

    bugfix: s3 var name

--- example/example.tf
+++ example/example.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_s3_bucket" "sftp" {
+resource "aws_s3_bucket" "s3_default" {
   bucket = "sftp-s3-bucket-test"
 }
 
@@ -15,6 +15,6 @@ module "sftp" {
   enable_sftp   = true
   public_key    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDgNHjv9nLigWwal+dY5UiAlLFsVeBK2QKGveI3lMTR6Oj/WrGzCNJB5vMMubHmKg5dZvz5cSoSBa5gJ3ViGVYS47k/jZl/5mZDB0gIrQlbW/JWmmoDMqtYEv43vAMhNXzCCeB/giWG5G9iC0fJS/6JjWFDAkIVaEx+aNPPYZabsv6nnIp48zoKeseWjks+/duk7uEb9TgQkgRUS959RZzfBOn1mmuKDwtQz5wh3Ev7ws5wZzSQBAZw1S1EbX/HOMLhoTYuJmNAZQvMg8iZMszjRfJwtwrwJk3YccuxF+IzVrIthfIAQSHBysLJ3/MWoRvF5hkxPJa/u7Xg3oPApSNm1HPlQ1MqQCX1KyJYEjhFMfEj19vRckFKdb6U9WbOh5YADbrRrvqvfXX1pWfHb3Pe7ykKU0RzsVTc6oTV65RbfqW7Wb7MDKtfuj1gg+H72cYP7792pZkgOOtgFqbguJidk0ftt4Ls7UZALVClBt3Mw9sdqZXt1i/BumADjSDu97M= 82789@Ryan-Desktop"
   user_name     = "ftp-user"
-  s3_bucket_id  = "sftp-bucket"
+  s3_bucket_id  = "sftp-s3-bucket-test"
   endpoint_type = "PUBLIC"
 }
